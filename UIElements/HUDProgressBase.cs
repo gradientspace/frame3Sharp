1be4d0df65985c3489885259f9a9f3e1e6848e3f
rms <rms@rms80.com>
2017-01-16 15:22:10 -0500

    base abstract functionality for progress UI element.

---
+++ UIElements/HUDProgressBase.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using g3;
+
+namespace f3
+{
+    public class HUDProgressBase : HUDStandardItem
+    {
+        protected fGameObject rootGO;
+
+        public virtual void Create()
+        {
+            rootGO = new GameObject(UniqueNames.GetNext("HUDProgress"));
+        }
+
+        public event ValueChangedHandler OnProgressChanged;
+        public event EndValueChangeHandler OnProgressCompleted;
+
+        double current_progress;
+
+        double max_progress = 1.0;
+
+        public double Progress {
+            get { return current_progress; }
+            set {
+                if (current_progress != value)
+                    update_progress(value, true);
+            }
+        }
+
+        public double MaxProgress {
+            get { return max_progress; }
+        }
+
+
+        void update_progress(double newValue, bool bSendEvent)
+        {
+            if (newValue != current_progress) {
+                double prev = current_progress;
+                current_progress = newValue;
+                update_geometry();
+                if (bSendEvent) {
+                    UnityUtil.SafeSendEvent(OnProgressChanged, this, prev, current_progress);
+                    if ( current_progress == max_progress )
+                        UnityUtil.SafeSendEvent(OnProgressCompleted, this, max_progress);
+
+                }
+            }
+        }
+
+
+        protected virtual void update_geometry()
+        {
+            // subclass should override this to be notified that it is time to update internals
+        }
+
+
+
+		#region SceneUIElement implementation
+
+		override public GameObject RootGameObject {
+			get { return rootGO; }
+		}
+
+        override public bool WantsCapture(InputEvent e)
+        {
+            return false;
+        }
+
+        override public bool BeginCapture (InputEvent e)
+		{
+            return false;
+		}
+
+		override public bool UpdateCapture (InputEvent e)
+		{
+            return false;
+		}
+
+		override public bool EndCapture (InputEvent e)
+		{
+			return false;
+		}
+
+		#endregion
+    }
+}
