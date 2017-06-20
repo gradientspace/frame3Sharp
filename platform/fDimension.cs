0ee24ff2cafe8bea766b8a663ea0325526f69e9f
rms <rms@rms80.com>
2017-06-20 09:53:26 -0400

    added fPosition and fDimension, which allow for representing a linear dimension or 3D position in multiple coordinate space simultaneously. Added SceneTransform functions for dimensions

---
+++ platform/fDimension.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using g3;
+
+namespace f3
+{
+    public enum DimensionType
+    {
+        //Pixels,       // not implemented yet
+        WorldUnits, SceneUnits,
+        //SOUnits,      // not implemented yet
+        VisualAngle
+    }
+
+
+    /// <summary>
+    /// fDimension represents a dimension in one of multiple sets of units.
+    /// The purpose of this is to handle UI sizing where we want to size the same
+    /// objects by different units in different situations.
+    /// 
+    /// For example in VR we might want to specify a label height by visual angle,
+    /// while on desktop we might want to specify pixels.
+    /// </summary>
+    public abstract class fDimension
+    {
+        DimensionType type;
+        public DimensionType Type {
+            get { return type; }
+        }
+
+        public abstract double WorldValue { get; }
+        public abstract double SceneValue { get; }
+
+        public float WorldValuef {
+            get { return (float)WorldValue; }
+        }
+        public float SceneValuef {
+            get { return (float)SceneValue; }
+        }
+
+
+        protected fDimension(DimensionType eType)
+        {
+            type = eType;
+        }
+
+
+        protected FScene ActiveScene {
+            get { return FScene.Active; }       // [TODO] this is bad!!
+        }
+
+
+        // simplify constructors
+        public static fDimension World(double fWorld) { return new fWorldDimension(fWorld); }
+        public static fDimension World(Func<double> worldF) { return new fDynamicWorldDimension(worldF); }
+        public static fDimension Scene(double fScene) { return new fSceneDimension(fScene); }
+        public static fDimension Scene(Func<double> sceneF) { return new fDynamicSceneDimension(sceneF); }
+        public static fDimension VisualAngle(Vector3f position, double fDegrees) { return new fVisualAngleDimension(position, fDegrees); }
+        public static fDimension VisualAngle(Func<Vector3f> positionF, double fDegrees) { return new fDynamicVisualAngleDimension(positionF, fDegrees); }
+        public static fDimension VisualAngle(fPosition position, double fDegrees) { return new fPositionVisualAngleDimension(position, ()=> { return fDegrees; } ); }
+    }
+
+
+
+
+    public abstract class fConstantDimension : fDimension
+    {
+        protected double value;
+        protected fConstantDimension(DimensionType eType, double fValue) : base(eType) {
+            value = fValue;
+        }
+    }
+
+
+    public class fWorldDimension : fConstantDimension
+    {
+        public fWorldDimension(double fWorld) : base(DimensionType.WorldUnits, fWorld)
+        {
+        }
+
+        public override double WorldValue {
+            get { return base.value; }
+        }
+        public override double SceneValue {
+            get { return ActiveScene.ToSceneDimension((float)base.value); }
+        }
+    }
+
+
+
+    public class fDynamicWorldDimension : fDimension
+    {
+        Func<double> worldValueF;   
+
+        public fDynamicWorldDimension(Func<double> worldF) : base(DimensionType.WorldUnits)
+        {
+            worldValueF = worldF;
+        }
+
+        public override double WorldValue {
+            get { return worldValueF();}
+        }
+        public override double SceneValue {
+            get { return ActiveScene.ToSceneDimension((float)worldValueF()); }
+        }
+    }
+
+
+
+
+    public class fSceneDimension : fConstantDimension
+    {
+        public fSceneDimension(double fWorld) : base(DimensionType.SceneUnits, fWorld)
+        {
+        }
+
+        public override double WorldValue {
+            get { return ActiveScene.ToWorldDimension((float)base.value); }
+        }
+        public override double SceneValue {
+            get { return base.value; }
+        }
+    }
+
+
+    public class fDynamicSceneDimension : fDimension
+    {
+        Func<double> sceneValueF;   
+
+        public fDynamicSceneDimension(Func<double> sceneF) : base(DimensionType.SceneUnits)
+        {
+            sceneValueF = sceneF;
+        }
+
+        public override double WorldValue {
+            get { return ActiveScene.ToWorldDimension((float)sceneValueF()); }
+        }
+        public override double SceneValue {
+            get { return sceneValueF(); }
+        }
+    }
+
+
+
+    public class fVisualAngleDimension : fConstantDimension
+    {
+        Vector3f position;
+        public virtual Vector3f Position {
+            get { return position; }
+        }
+
+        public fVisualAngleDimension(Vector3f position, double fValue) : base(DimensionType.VisualAngle, fValue)
+        {
+            this.position = position;
+        }
+
+        public override double WorldValue {
+            get {
+                return VRUtil.GetVRRadiusForVisualAngle(Position,
+                    ActiveScene.ActiveCamera.GetPosition(), (float)base.value);
+            }
+        }
+        public override double SceneValue {
+            get { return ActiveScene.ToSceneDimension((float)WorldValue); }
+        }
+    } 
+
+
+
+
+
+    public class fDynamicVisualAngleDimension : fConstantDimension
+    {
+        Func<Vector3f> positionF;   
+        public virtual Vector3f Position {
+            get { return positionF(); }
+        }
+
+        public fDynamicVisualAngleDimension(Func<Vector3f> positionF, double fValue) : base(DimensionType.VisualAngle, fValue)
+        {
+            this.positionF = positionF;
+        }
+
+        public override double WorldValue {
+            get {
+                return VRUtil.GetVRRadiusForVisualAngle(Position,
+                    ActiveScene.ActiveCamera.GetPosition(), (float)base.value);
+            }
+        }
+        public override double SceneValue {
+            get { return ActiveScene.ToSceneDimension((float)WorldValue); }
+        }
+    } 
+
+
+
+
+    public class fPositionVisualAngleDimension : fDimension
+    {
+        public fPosition Position;
+        Func<double> AngleF;
+
+        public fPositionVisualAngleDimension(fPosition pos, Func<double> angleF) : base(DimensionType.VisualAngle)
+        {
+            this.Position = pos;
+            this.AngleF = angleF;
+        }
+
+        public override double WorldValue {
+            get {
+                return VRUtil.GetVRRadiusForVisualAngle(Position.WorldPoint,
+                    ActiveScene.ActiveCamera.GetPosition(), (float)AngleF());
+            }
+        }
+        public override double SceneValue {
+            get { return ActiveScene.ToSceneDimension((float)WorldValue); }
+        }
+    } 
+
+
+
+}
---
+++ platform/fPosition.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using g3;
+
+namespace f3
+{
+
+    public enum PositionType
+    {
+        World,
+        Scene
+        //, SOPosition
+    }
+
+
+    /// <summary>
+    /// fPosition represents a position in one of multiple coordinate systems.
+    /// The purpose of this is to handle cases where we want to use a point in
+    /// multiple coordinate systems. So, a point can be specified in World but
+    /// evaluated in Scene, etc.
+    /// </summary>
+    public abstract class fPosition
+    {
+        PositionType type;
+        public PositionType Type {
+            get { return type; }
+        }
+        
+        public abstract Vector3f WorldPoint { get; }
+        public abstract Vector3f ScenePoint { get; }
+
+        protected fPosition(PositionType eType)
+        {
+            type = eType;
+        }
+
+
+        protected FScene ActiveScene {
+            get { return FScene.Active; }       // [TODO] this is bad!!
+        }
+
+
+        public static fPosition World(Vector3f fWorld) { return new fWorldPosition(fWorld); }
+        public static fPosition World(Func<Vector3f> worldF) { return new fDynamicWorldPosition(worldF); }
+        public static fPosition Scene(Vector3f fScene) { return new fWorldPosition(fScene); }
+        public static fPosition Scene(Func<Vector3f> sceneF) { return new fDynamicScenePosition(sceneF); }
+
+    }
+
+
+    public abstract class fConstantPosition : fPosition
+    {
+        protected Vector3f point;
+        protected fConstantPosition(PositionType eType, Vector3f pos) : base(eType) {
+            point = pos;
+        }
+    }
+
+
+    public class fWorldPosition : fConstantPosition
+    {
+        public fWorldPosition(Vector3f pt) : base(PositionType.World, pt)
+        {
+        }
+
+        public override Vector3f WorldPoint {
+            get { return base.point; }
+        }
+        public override Vector3f ScenePoint {
+            get { return ActiveScene.ToSceneP(base.point); }
+        }
+    }
+
+
+
+    public class fDynamicWorldPosition : fPosition
+    {
+        Func<Vector3f> worldPointF;   
+
+        public fDynamicWorldPosition(Func<Vector3f> worldF) : base(PositionType.World) {
+            worldPointF = worldF;
+        }
+
+        public override Vector3f WorldPoint {
+            get { return worldPointF(); }
+        }
+        public override Vector3f ScenePoint {
+            get { return ActiveScene.ToSceneP(worldPointF()); }
+        }
+    }
+
+
+
+
+
+    public class fScenePosition : fConstantPosition
+    {
+        public fScenePosition(Vector3f pt) : base(PositionType.Scene, pt)
+        {
+        }
+
+        public override Vector3f WorldPoint {
+            get { return ActiveScene.ToWorldP(base.point); }
+        }
+        public override Vector3f ScenePoint {
+            get { return base.point; }
+        }
+    }
+
+
+
+    public class fDynamicScenePosition : fPosition
+    {
+        Func<Vector3f> scenePointF;   
+
+        public fDynamicScenePosition(Func<Vector3f> sceneF) : base(PositionType.Scene) {
+            scenePointF = sceneF;
+        }
+
+        public override Vector3f WorldPoint {
+            get { return ActiveScene.ToWorldP(scenePointF()); }
+        }
+        public override Vector3f ScenePoint {
+            get { return scenePointF(); }
+        }
+    }
+
+
+
+}
