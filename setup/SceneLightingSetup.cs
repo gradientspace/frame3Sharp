6299d126ed6db70110ccc96caffd95035f184532
rms <rms@rms80.com>
2017-07-19 23:09:50 -0400

    tweaks to default SceneLightingSetup - more configurable, shadow dist works better, etc. Other apps may need update though.

--- setup/SceneLightingSetup.cs
+++ setup/SceneLightingSetup.cs
@@ -18,26 +18,38 @@ namespace f3 {
         // May be too far (or too close) for your scene!
         public float LightDistance = 20.0f;
 
+        public float LightAltitudeAngle = 45.0f;
+
+        public int LightCount = 4;
+
+        public int ShadowLightCount = 2;
+
+
         List<GameObject> lights = new List<GameObject>();
 
 		// Use this for initialization
 		void Start () {
-			Vector3 vCornerPos = LightDistance * new Vector3 (1, 3, 1).normalized;
+            Vector3f xz = new Vector3f(1, 0, 1); xz.Normalize();
+            Vector3f angleVec = Quaternionf.AxisAngleD(new Vector3f(xz.x, 0, -xz.z), -LightAltitudeAngle) * xz;
+            //Vector3f angleVec = new Vector3f(1, 3, 1);
+            angleVec.Normalize();
+			Vector3f vCornerPos = LightDistance * angleVec;
 
             // create a set of overheard spotlights
-			for (int k = 0; k < 4; k++) {
+            float rotAngle = 360.0f / (float)LightCount;
+			for (int k = 0; k < LightCount; k++) {
 				GameObject lightObj = new GameObject (string.Format ("spotlight{0}", k));
 				Light lightComp = lightObj.AddComponent<Light> ();
 				lightComp.type = LightType.Directional;
 				lightComp.transform.position = vCornerPos;
 				lightComp.transform.LookAt (Vector3.zero);
-				lightComp.transform.RotateAround(Vector3.zero, Vector3.up, (float)k * 90.0f);
+				lightComp.transform.RotateAround(Vector3.zero, Vector3.up, (float)k * rotAngle);
 
 				lightComp.intensity = 0.1f;
 				lightComp.color = Color.white;
 
                 // only add shadows on first two lights
-				if ( k == 0 || k == 1 )
+				if ( k < ShadowLightCount )
 					lightComp.shadows = LightShadows.Hard;
 
 				lightComp.transform.SetParent ( this.gameObject.transform );
@@ -68,8 +80,18 @@ namespace f3 {
             Vector3f thisW = lights[0].transform.position;
             float fHeight =
                 Vector3f.Dot(( thisW - sceneFrameW.Origin), sceneFrameW.Y);
+            float fShadowDist = fHeight * 1.5f;
 
-            int nShadowDist = (int)Mathf.Round( fHeight * 1.5f );
+            // lights need to be in-range
+            if (fShadowDist < LightDistance)
+                fShadowDist = LightDistance * 1.5f;
+
+            // need to be a multiple of eye distance
+            float fEyeDist = sceneFrameW.Origin.Distance(Camera.main.transform.position);
+            fShadowDist = Mathf.Max(fShadowDist, 1.5f * fEyeDist);
+
+            int nShadowDist = (int)Snapping.SnapToIncrement(fShadowDist, 50);
+
             if (cur_shadow_dist != nShadowDist) {
                 QualitySettings.shadowDistance = nShadowDist;
                 cur_shadow_dist = nShadowDist;
