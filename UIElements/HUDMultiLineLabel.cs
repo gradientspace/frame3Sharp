434b362b5d5271bf4d205e9a22493dcbf2dbc6ac
rms <rms@rms80.com>
2017-07-31 15:04:54 -0400

    add HUDMultiLineLabel, HUDPopupDialog

---
+++ UIElements/HUDMultiLineLabel.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using g3;
+
+namespace f3
+{
+    // NB: on creation, this button is oriented so that positive Z points away from the *back* of
+    //  this field (ie the field "faces" -Z). 
+    public class HUDMultiLineLabel : HUDStandardItem, IBoxModelElement
+    {
+        fGameObject entry;
+        fGameObject bgMesh;
+        fGameObject border;
+        fTextAreaGameObject textMesh;
+
+        public HUDShape Shape { get; set; }
+
+        public float TextHeight { get; set; }
+
+        Colorf bg_color;
+        public Colorf BackgroundColor {
+            get { return bg_color; }
+            set { bg_color = value;  UpdateText(); }
+        }
+
+        Colorf text_color;
+        public Colorf TextColor {
+            get { return text_color; }
+            set { text_color = value; UpdateText(); }
+        }
+
+        Colorf disabled_text_color;
+        public Colorf DisabledTextColor {
+            get { return disabled_text_color; }
+            set { disabled_text_color = value; UpdateText(); }
+        }
+
+
+        // [TODO] expose these properly
+        public bool EnableBorder = false;
+        public float BorderWidth = 1.0f;
+        public Colorf BorderColor = Colorf.DarkSlateGrey;
+
+
+
+        public HorizontalAlignment AlignmentHorz { get; set; }
+
+        string text;
+        public string Text
+        {
+            get { return text; }
+            set { text = value; UpdateText(); }
+        }
+
+        public HUDMultiLineLabel()
+        {
+            Shape = new HUDShape(HUDShapeType.Rectangle, 10, 1);
+            TextHeight = 0.8f;
+
+            BackgroundColor = Colorf.VideoWhite;
+            TextColor = Colorf.VideoBlack;
+            DisabledTextColor = Colorf.DimGrey;
+            AlignmentHorz = HorizontalAlignment.Left;
+            text = "(text)";
+        }
+
+
+        // creates a button in the desired geometry shape
+        public void Create()
+        {
+            entry = GameObjectFactory.CreateParentGO(UniqueNames.GetNext("HUDMultiLineLabel"));
+
+            bgMesh = new fGameObject(AppendMeshGO("background", HUDUtil.MakeBackgroundMesh(Shape),
+                MaterialUtil.CreateFlatMaterialF(BackgroundColor),
+                entry));
+            bgMesh.RotateD(Vector3f.AxisX, -90.0f);
+
+
+            if ( EnableBorder ) {
+                HUDShape borderShape = Shape;
+                borderShape.Radius += BorderWidth;
+                borderShape.Height += 2 * BorderWidth;
+                borderShape.Width += 2 * BorderWidth;
+                border = new fGameObject(AppendMeshGO("border", HUDUtil.MakeBackgroundMesh(borderShape),
+                    MaterialUtil.CreateFlatMaterialF(BorderColor), entry));
+                border.RotateD(Vector3f.AxisX, -90.0f);
+                border.Translate(-0.001f * Vector3f.AxisY, true);
+            }
+
+            textMesh = GameObjectFactory.CreateTextAreaGO(
+                    "text", Text, TextColor, TextHeight, Shape.Size, AlignmentHorz, 
+                    BoxPosition.TopLeft, SceneGraphConfig.TextLabelZOffset );
+            textMesh.TextObject.SetOverflowMode(TextOverflowMode.Ellipses);
+
+            BoxPosition horizontalBoxPos = BoxPosition.TopLeft;
+            if (AlignmentHorz == HorizontalAlignment.Center)
+                horizontalBoxPos = BoxPosition.CenterTop;
+            else if (AlignmentHorz == HorizontalAlignment.Right)
+                horizontalBoxPos = BoxPosition.TopRight;
+            Vector2f toPos = BoxModel.GetBoxPosition(this, horizontalBoxPos);
+            BoxModel.Translate(textMesh, Vector2f.Zero, toPos);
+
+            AppendNewGO(textMesh, entry, false);
+        }
+
+        void UpdateText()
+        {
+            if ( bgMesh != null ) {
+                bgMesh.SetColor(BackgroundColor);
+            }
+            if (textMesh != null) {
+                textMesh.SetColor( (Enabled) ? TextColor : DisabledTextColor );
+                textMesh.SetText(Text);
+            }
+        }
+
+        // events for clicked and double-clicked.
+        // NOTE: for doubleclick you will always receive an [OnClicked,OnDoubleClicked] pair.
+        //    (alternative would be to delay OnClicked...perhaps that should be added as an option)
+        public event EventHandler OnClicked;
+        public event EventHandler OnDoubleClicked;
+
+
+
+        protected override void OnEnabledChanged()
+        {
+            base.OnEnabledChanged();
+            UpdateText();
+        }       
+
+
+
+        #region SceneUIElement implementation
+
+        override public fGameObject RootGameObject
+        {
+            get { return entry; }
+        }
+
+        override public bool WantsCapture(InputEvent e)
+        {
+            return (Enabled && HasGO(e.hit.hitGO));
+        }
+
+        override public bool BeginCapture(InputEvent e)
+        {
+            return true;
+        }
+
+        override public bool UpdateCapture(InputEvent e)
+        {
+            return true;
+        }
+
+        bool sent_click = false;
+        float last_click_time = 0.0f;
+        float double_click_delay = 0.3f;
+
+
+        override public bool EndCapture(InputEvent e)
+        {
+            if (FindHitGO(e.ray)) {
+
+                if (sent_click == false) {
+                    // on first click we reset timer
+                    FUtil.SafeSendEvent(OnClicked, this, new EventArgs());
+                    sent_click = true;
+                    last_click_time = FPlatform.RealTime();
+                } else {
+                    float delta = FPlatform.RealTime() - last_click_time;
+                    if (delta < double_click_delay) {
+                        // if this second click comes fast enough, send doubleclick instead
+                        FUtil.SafeSendEvent(OnDoubleClicked, this, new EventArgs());
+                        sent_click = false;
+                        last_click_time = 0.0f;
+                    } else {
+                        // send single-click and reset timer
+                        FUtil.SafeSendEvent(OnClicked, this, new EventArgs());
+                        sent_click = true;
+                        last_click_time = FPlatform.RealTime();
+                    }
+                }
+
+
+            }
+            return true;
+        }
+
+        #endregion
+
+
+
+
+       #region IBoxModelElement implementation
+
+
+        public Vector2f Size2D {
+            get { return new Vector2f(Shape.Width, Shape.Height); }
+        }
+
+        public AxisAlignedBox2f Bounds2D { 
+            get {
+                Vector2f origin2 = RootGameObject.GetLocalPosition().xy;
+                return new AxisAlignedBox2f(origin2, Shape.Width/2, Shape.Height/2);
+            }
+        }
+
+
+        #endregion
+
+
+    }
+
+}
---
+++ UIElements/HUDPopupDialog.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using g3;
+
+namespace f3
+{
+    /// <summary>
+    /// Basic popup dialog box, with arbitrary set of buttons
+    /// Note that most fields must be configured before calling Create()
+    /// </summary>
+    public class HUDPopupDialog : HUDPanel
+    {
+        HUDShapeElement background;
+        HUDLabel header_label;
+        HUDMultiLineLabel message_area;
+        List<HUDLabel> buttons;
+
+        // Styling
+
+        public Func<float, float, HUDShape> BackgroundShapeF = HUDUIDefaults.MakeDialogBackgroundShapeF;
+        public Colorf BackgroundColor { get; set; }
+
+
+        public virtual HUDLabelStyle HeaderStyle {
+            get { return header_style; }
+            set { header_style = value; update_header_style(); }
+        }
+        HUDLabelStyle header_style = new HUDLabelStyle();
+
+
+        public virtual HUDLabelStyle MessageStyle
+        {
+            get { return message_style; }
+            set { message_style = value; update_message_style(); }
+        }
+        HUDLabelStyle message_style = new HUDLabelStyle();
+
+
+        public virtual HUDButtonStyle ButtonStyle {
+            get { return button_style; }
+            set { button_style = value; update_button_style(); }
+        }
+        HUDButtonStyle button_style = new HUDButtonStyle();
+
+
+        // Text strings
+
+        string titleText;
+        public string TitleText
+        {
+            get { return titleText; }
+            set { titleText = value; UpdateText(); }
+        }
+
+        string messageText;
+        public string MessageText
+        {
+            get { return messageText; }
+            set { messageText = value; UpdateText(); }
+        }
+
+
+        public HUDPopupDialog()
+        {
+            Width = 10;
+            Height = 5;
+
+            BackgroundColor = Colorf.Silver;
+            header_style.TextHeight = 4 * message_style.TextHeight;
+            header_style.TextColor = Colorf.DarkSlateGrey;
+
+            titleText = "Question?";
+            messageText = "This is a popup message!";
+
+            buttons = new List<HUDLabel>();
+        }
+
+
+
+
+        public virtual void AppendButton(string label, Action clickedF)
+        {
+            HUDLabel button = new HUDLabel(ButtonStyle, ButtonStyle) {
+                Text = label
+            };
+            button.OnClicked += (s, e) => { clickedF(); };
+            buttons.Add(button);
+        }
+
+
+
+
+        public override void Create()
+        {
+            base.Create();
+
+            HUDElementLayout layout = new HUDElementLayout(this, new HUDPanelContentBox(this), this.Children);
+            float fZ = -0.01f * Width;
+            //Vector2f vertFieldOffset = -FieldSpacing * Vector2f.AxisY;
+            //Vector2f horzFieldOffset = FieldSpacing * Vector2f.AxisX;
+
+            background = new HUDShapeElement() {
+                Shape = BackgroundShapeF(this.Width, this.Height),
+                Color = this.BackgroundColor
+            };
+            background.Create();
+            background.Name = "background";
+            layout.Add(background, new LayoutOptions() { Flags = LayoutFlags.None,
+                PinSourcePoint2D = LayoutUtil.BoxPointF(background, BoxPosition.Center),
+                PinTargetPoint2D = LayoutUtil.BoxPointF(layout.BoxElement, BoxPosition.Center)
+            });
+
+            
+            header_label = new HUDLabel() {
+                Shape = new HUDShape(HUDShapeType.Rectangle, this.PaddedWidth, HeaderStyle.TextHeight),
+                TextHeight = HeaderStyle.TextHeight,
+                BackgroundColor = Colorf.TransparentBlack,
+                TextColor = HeaderStyle.TextColor,
+                AlignmentHorz = HeaderStyle.AlignmentHorz,
+                Text = TitleText
+            };
+            header_label.Create();
+            header_label.Name = "header_label";
+            BoxPosition headerBoxPos = BoxModel.ToPosition(HeaderStyle.AlignmentHorz, VerticalAlignment.Top);
+            layout.Add(header_label, 
+                LayoutUtil.PointToPoint(header_label, headerBoxPos, layout.BoxElement, headerBoxPos, fZ) );
+
+
+            float message_height = this.PaddedHeight - HeaderStyle.TextHeight - ButtonStyle.Height;
+
+
+            message_area = new HUDMultiLineLabel() {
+                Shape = new HUDShape(HUDShapeType.Rectangle, this.PaddedWidth, message_height),
+                TextHeight = MessageStyle.TextHeight,
+                BackgroundColor = Colorf.TransparentBlack,
+                TextColor = MessageStyle.TextColor,
+                AlignmentHorz = MessageStyle.AlignmentHorz,
+                Text = this.MessageText
+            };
+            message_area.Create();
+            message_area.Name = "message_area";
+            BoxPosition messageAreaBoxPos = BoxModel.ToPosition(MessageStyle.AlignmentHorz, VerticalAlignment.Top);
+            layout.Add(message_area, 
+                LayoutUtil.PointToPoint(message_area, messageAreaBoxPos, header_label, BoxModel.ToBottom(headerBoxPos), fZ) );
+
+
+            IBoxModelElement prev = layout.BoxElement;
+            BoxPosition prevPos = BoxPosition.BottomRight;
+            Vector2f prevShift = Vector2f.Zero;
+            for ( int i = buttons.Count-1; i >= 0; --i ) {
+                buttons[i].Create();
+                buttons[i].Name = buttons[i].Text;
+                layout.Add(buttons[i],
+                    LayoutUtil.PointToPoint(buttons[i], BoxPosition.BottomRight, prev, prevPos, prevShift, fZ));
+                prev = buttons[i];
+                prevPos = BoxPosition.BottomLeft;
+                prevShift = -Padding * Vector2f.AxisX;
+            }
+
+
+        }
+
+
+
+        void update_button_style()
+        {
+            // [TODO]
+        }
+        void update_header_style()
+        {
+        }
+        void update_message_style()
+        {
+        }
+        
+
+
+        public void Dismiss()
+        {
+            FUtil.SafeSendEvent(OnDismissed, this, new EventArgs());
+        }
+
+
+        void UpdateText()
+        {
+            //if ( bgMesh != null ) {
+            //    bgMesh.SetColor(BackgroundColor);
+            //}
+            //if (textMesh != null) {
+            //    textMesh.SetColor( TextColor );
+            //    textMesh.SetText(Text);
+            //}
+            //if (titleTextMesh != null) {
+            //    titleTextMesh.SetColor( TitleTextColor );
+            //    titleTextMesh.SetText(TitleText);
+            //}
+        }
+
+
+
+        // events for clicked and dismissed.
+        public event EventHandler OnDismissed;
+
+
+
+    }
+
+}
