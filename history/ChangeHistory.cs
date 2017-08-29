c891433ea3f98f304ab7c3050603300ca3179e8f
rms <rms@rms80.com>
2017-08-29 10:32:55 -0400

    history improvements, docs

--- history/ChangeHistory.cs
+++ history/ChangeHistory.cs
@@ -6,6 +6,9 @@ using System.Text;
 namespace f3
 {
 
+    /// <summary>
+    /// Status information for history Change nodes
+    /// </summary>
     public struct OpStatus
     {
         public const int no_error = 0;
@@ -27,20 +30,28 @@ namespace f3
     }
 
 
+    /// <summary>
+    /// Base interaface for all Change nodes that can be passed to ChangeHistory (ie undo/redo operations)
+    /// </summary>
     public interface IChangeOp
     {
         string Identifier();
-        List<string> GetTags();
 
-        OpStatus Apply();
-        OpStatus Revert();
-        OpStatus Cull();        // called when this operation is no longer needed
+        bool HasTags { get; }
+        List<string> Tags { get; set; }     // get must not return null if HasTags is true
+
+        OpStatus Apply();       // "redo" action
+        OpStatus Revert();      // "undo" action
+        OpStatus Cull();        // called when this operation is no longer needed, you should discard memory if possible
     }
 
 
     public delegate void OnChangeOpHandler(object source, IChangeOp op);
 
 
+    /// <summary>
+    /// Default implementation for changes, supports lazy-allocated tag set
+    /// </summary>
     public abstract class BaseChangeOp : IChangeOp
     {
         public abstract string Identifier();
@@ -48,15 +59,26 @@ namespace f3
         public abstract OpStatus Revert();
         public abstract OpStatus Cull();
 
-        public virtual List<string> GetTags() { return Tags; }
-        public List<string> Tags { get; set; }
+        public virtual bool HasTags {
+            get { return tags != null; }
+        }
+
+        public virtual List<string> Tags {
+            get { if (tags == null) tags = new List<string>(); return tags; }
+            set { tags = value; }
+        }
+        List<string> tags;
 
         public BaseChangeOp(bool bUseTags = false) {
             if (bUseTags)
-                Tags = new List<string>();
+                tags = new List<string>();
         }
     }
 
+    /// <summary>
+    /// special-case change operation, push one of these after interactive actions end
+    /// and then you can use ChangeHistory.InteractiveStepBack/Forward to implement normal undo/redo-type interaction
+    /// </summary>
     public class InteractionCheckpointOp : BaseChangeOp
     {
         public const string InteractiveStateTag = "//Interactive";
@@ -75,10 +97,18 @@ namespace f3
 
 
 
+    /// <summary>
+    /// Top-level class for managing a linear stream of change/history events, ie implementations of IChangeOp.
+    /// This is used to implement Undo/Redo functionality.
+    /// The idea is that you push Changes, and then you can StepForward/StepBackward, and we call Apply()/Revert()
+    /// on the Change nodes. To step forward/backward in chunks, Changes can be tagged with text strings.
+    /// InteractionCheckpointOp is a special-case tagged node that you can use at the "end" of an interactive operation.
+    /// </summary>
     public class ChangeHistory
     {
-        List<IChangeOp> vHistory;
-        int iCurrent;
+        List<IChangeOp> vHistory;       // stream of changes
+        int iCurrent;                   // current position in vHistory
+
 
         public ChangeHistory()
         {
@@ -87,6 +117,37 @@ namespace f3
         }
 
 
+        /// <summary>
+        /// History nodes can be tagged with text strings, this just generates a text string
+        /// that should be unique for this History stream.
+        /// </summary>
+        /// <returns></returns>
+        public string AllocateTag()
+        {
+            return "tag" + (iUniqueTagCounter++).ToString();
+        }
+        int iUniqueTagCounter = 1;
+
+        /// <summary>
+        /// If set to non-null/empty string, Active tag will be set on all pushed Changes
+        /// </summary>
+        public void SetActiveTag(string tag)
+        {
+            if (tag == null || tag.Length == 0)
+                throw new Exception("ChangeHistory.SetActiveTag: invalid tag string");
+            sForceSetTag = tag;
+        }
+        public void ClearActiveTag()
+        {
+            sForceSetTag = null;
+        }
+        string sForceSetTag = null;
+
+
+        /// <summary>
+        /// push Change onto the history stream. If we are not at end of stream, we truncate
+        /// stream at the current point. If bIsApplied is false, we Apply() the new Change
+        /// </summary>
         public OpStatus PushChange(IChangeOp op, bool bIsApplied = false)
         {
             DebugUtil.Log(2, "ChangeHistory.PushChange: pushed {0}", op.Identifier());
@@ -103,22 +164,30 @@ namespace f3
                 }
             }
 
+            if (sForceSetTag != null)
+                op.Tags.Add(sForceSetTag);
             vHistory.Add(op);
             iCurrent++;
 
             return OpStatus.Success;
         }
 
-        public void PushInteractionCheckpoint()
+        /// <summary>
+        /// PushChange with a new InteractionCheckpointOp
+        /// </summary>
+        public BaseChangeOp PushInteractionCheckpoint()
         {
-            PushChange(new InteractionCheckpointOp(), true);
+            InteractionCheckpointOp op = new InteractionCheckpointOp();
+            PushChange(op, true);
+            return op;
         }
 
 
         /// <summary>
-        /// If we are currently in a stepback state, we do not want to push repeat nodes.
+        /// If we are currently in a "past" state, we do not want to push repeat nodes.
         /// It isn't clear how to automatically prevent this, so you need to manually
         /// check that you are ! InPastState before pushing changes that you might have already pushed!!
+        /// (ie if your Change.Apply() or Revert() calls something that would emit a new ChangeOp)
         /// </summary>
         public bool InPastState
         {
@@ -126,6 +195,9 @@ namespace f3
         }
 
 
+        /// <summary>
+        /// Rewind history back one Change
+        /// </summary>
         public OpStatus StepBack()
         {
             if (iCurrent == 0)
@@ -143,6 +215,10 @@ namespace f3
             iCurrent--;
             return OpStatus.Success;
         }
+
+        /// <summary>
+        /// Rewind history until current node has the given tag
+        /// </summary>
         public OpStatus StepBackwardToAfterPreviousTag(string tag)
         {
             bool bContinue = true;
@@ -153,14 +229,17 @@ namespace f3
                 if (iCurrent == 0) {
                     bContinue = false;
                 } else {
-                    var tags = vHistory[iCurrent - 1].GetTags();
-                    if (tags != null && tags.Contains(tag))
+                    IChangeOp op = vHistory[iCurrent - 1];
+                    if (op.HasTags && op.Tags.Contains(tag))
                         bContinue = false;
                 }
             }
             return OpStatus.Success;
         }
 
+        /// <summary>
+        /// Rewind history until current node has the interactive tag
+        /// </summary>
         public OpStatus InteractiveStepBack()
         {
             return StepBackwardToAfterPreviousTag(InteractionCheckpointOp.InteractiveStateTag);
@@ -169,7 +248,9 @@ namespace f3
 
 
 
-
+        /// <summary>
+        /// play history forward one Change
+        /// </summary>
         public OpStatus StepForward()
         {
             if (iCurrent == vHistory.Count)
@@ -187,6 +268,10 @@ namespace f3
             iCurrent++;
             return OpStatus.Success;
         }
+
+        /// <summary>
+        /// play history forward until current node has the given tag
+        /// </summary>
         public OpStatus StepForwardToAfterNextTag(string tag)
         {
             bool bContinue = true;
@@ -197,13 +282,17 @@ namespace f3
                 if (iCurrent == vHistory.Count) {
                     bContinue = false;
                 } else {
-                    var tags = vHistory[iCurrent].GetTags();
-                    if (tags != null && tags.Contains(tag))
+                    IChangeOp op = vHistory[iCurrent];
+                    if (op.HasTags && op.Tags.Contains(tag))
                         bContinue = false;
                 }
             }
             return OpStatus.Success;
         }
+
+        /// <summary>
+        /// play history forward until current node has the interactive tag
+        /// </summary>
         public OpStatus InteractiveStepForward()
         {
             return StepForwardToAfterNextTag(InteractionCheckpointOp.InteractiveStateTag);
@@ -211,7 +300,33 @@ namespace f3
 
 
 
+        /// <summary>
+        /// discard any nodes with given tag. Note that this could result in invalid/garbge history,
+        /// preventing that is up to you.
+        /// </summary>
+        public void DiscardByTag(string tag)
+        {
+            for ( int i = 0; i < vHistory.Count; ++i ) {
+                if (vHistory[i].HasTags == false)
+                    continue;
 
+                if ( vHistory[i].Tags.Contains(tag) ) {
+                    if (i >= iCurrent)
+                        throw new Exception("ChangeHistory.DiscardByTag: cannot discard current or future states!");
+                    vHistory[i].Cull();
+                    vHistory.RemoveAt(i);
+                    i--;
+                    iCurrent--;
+                }
+            }
+        }
+
+
+
+        /// <summary>
+        /// Discard all future history nodes after current state. Called automatically
+        /// when you push a node while at iCurrent < Count
+        /// </summary>
         void TrimFuture()
         {
             if (iCurrent < vHistory.Count) {
@@ -221,6 +336,30 @@ namespace f3
             }
             if (iCurrent > vHistory.Count)
                 throw new Exception("ChangeHistory.TrimFuture: iCurrent points into non-existent future!");
+        }
+
+
+
+
+
+        public void DebugPrint(string prefix = "HISTORY")
+        {
+            StringBuilder s = new StringBuilder();
+
+
+            s.AppendLine(string.Format("[{0}] have {1} records, current is {2}", prefix, vHistory.Count, iCurrent));
+            for (int i = vHistory.Count-1; i >= 0; --i) {
+                string tags = "";
+                if ( vHistory[i].HasTags ) {
+                    foreach (string tag in vHistory[i].Tags)
+                        tags += tag + " ";
+                }
+                string current = (i == iCurrent) ? " **" : "   ";
+
+                s.AppendLine(string.Format("{0}[{1}] - {2} - tags: {3}", current, vHistory.Count-1-i, vHistory[i].Identifier(), tags));
+            }
+
+            DebugUtil.Log(2, s.ToString());
         }
 
 
