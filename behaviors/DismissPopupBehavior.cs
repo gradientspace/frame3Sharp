6674e396a768f9401784c83915bd37cb73a0bff2
rms <rms@rms80.com>
2017-09-05 21:43:27 -0400

    behaviors for implementing context popup menus (dismiss-on-input, wheel scroll)

---
+++ behaviors/DismissPopupBehavior.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using g3;
+
+namespace f3
+{
+    /// <summary>
+    /// Fires DimissF if PopupHitTestF fails on input pressed. Works for any input type.
+    /// Primary use case is to dismiss a popup like a context menu.
+    /// </summary>
+    public class DismissPopupBehavior : StandardInputBehavior
+    {
+        public Action DismissF = null;
+        public Func<Ray3f, bool> PopupHitTestF = null;
+
+        public DismissPopupBehavior()
+        {
+            Priority = 0;
+        }
+
+        public override InputDevice SupportedDevices { get {
+                return InputDevice.AnySpatialDevice | InputDevice.Mouse | InputDevice.Gamepad | InputDevice.TabletFingers;
+            }
+        }
+
+
+        public override CaptureRequest WantsCapture(InputState input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Capture BeginCapture(InputState input, CaptureSide eSide)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Capture UpdateCapture(InputState input, CaptureData data)
+        {
+            if ( input.IsForDevice(InputDevice.Mouse) ) {
+                if (input.bLeftMousePressed || input.bMiddleMousePressed || input.bRightMousePressed) {
+                    if (PopupHitTestF(input.vMouseOrthoWorldRay) == false)
+                        DismissF();
+                }
+            } else if ( input.IsForDevice(InputDevice.TabletFingers) ) { 
+                if ( input.bTouchPressed ) {
+                    if (PopupHitTestF(input.vTouchOrthoWorldRay) == false)
+                        DismissF();
+                }
+            } else if ( input.IsForDevice(InputDevice.AnySpatialDevice) ) {
+                if (input.bLeftTriggerPressed && PopupHitTestF(input.vLeftSpatialWorldRay) == false) {
+                    DismissF();
+                } else if (input.bRightTriggerPressed && PopupHitTestF(input.vRightSpatialWorldRay) == false) {
+                    DismissF();
+                }
+            }
+           
+            return Capture.Continue;
+        }
+
+        public override Capture ForceEndCapture(InputState input, CaptureData data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ behaviors/MouseWheelRegionBehavior.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using g3;
+
+namespace f3
+{
+    /// <summary>
+    /// Applies MouseWheelF if RegionHitTest passes and you spun the mouse wheel.
+    /// Region could be for example the ray-hit test of a context menu popup.
+    /// This is only meant to be used as an Override behavior, as wheel does not
+    /// provide pressed/release so we can't do normal capture process.
+    /// </summary>
+    public class MouseWheelRegionBehavior : StandardInputBehavior
+    {
+        public Action<float> MouseWheelF = null;
+        public Func<Ray3f, bool> RegionHitTest = null;
+
+        public MouseWheelRegionBehavior()
+        {
+            Priority = 0;
+        }
+
+        public override InputDevice SupportedDevices { get {
+                return InputDevice.Mouse;
+            }
+        }
+
+        public override CaptureRequest WantsCapture(InputState input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Capture BeginCapture(InputState input, CaptureSide eSide)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Capture UpdateCapture(InputState input, CaptureData data)
+        {
+            if (input.fMouseWheel == 0)
+                return Capture.Continue;
+
+            if ( input.IsForDevice(InputDevice.Mouse) ) {
+                if (RegionHitTest(input.vMouseOrthoWorldRay)) {
+                    MouseWheelF(input.fMouseWheel);
+                    return Capture.End;
+                }
+            }
+           
+            return Capture.Continue;
+        }
+
+        public override Capture ForceEndCapture(InputState input, CaptureData data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
