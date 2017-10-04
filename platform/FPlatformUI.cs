194e19bdb5e8e0707f5612de34075573fc5ab502
rms <rms@rms80.com>
2017-10-04 18:46:03 -0400

    platform-specific UI functions (will there be more? maybe should just be in FPlatform?)

---
+++ platform/FPlatformUI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+
+namespace f3
+{
+    public static class FPlatformUI
+    {
+        public static bool IsConsumingMouseInput()
+        {
+            // [RMS] this works?
+            bool over_go = UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject();
+            return over_go;
+        }
+    }
+}
