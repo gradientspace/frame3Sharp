e3d860ff3547b707b6d16c3bedcc00ac1858d78d
rms <rms@rms80.com>
2017-11-02 20:40:28 -0400

    catch exception when setting cwd in open-file dialog

--- FPlatform.cs
+++ FPlatform.cs
@@ -42,7 +42,9 @@ namespace f3
         public static string TemporaryDataPath() {
             return Application.temporaryCachePath;
         }
-
+        public static string GameExecutablePath() {
+            return Path.GetFullPath(Path.Combine(Application.dataPath, ".."));
+        }
 
 
         private static float _last_realtime = 0;
@@ -253,7 +255,11 @@ namespace f3
                 filterPatterns.Length, filterPatterns, sPatternDesc, 0);
 
             ShowingExternalPopup = false;
-            Directory.SetCurrentDirectory(curDirectory);
+            try {
+                Directory.SetCurrentDirectory(curDirectory);
+            } catch (Exception) {
+                // [RMS] sometimes this results in an exception? I am confused...
+            }
 
             if (p == IntPtr.Zero)
                 return null;
