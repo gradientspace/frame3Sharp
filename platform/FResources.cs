6ddb0c35959baf3df34f1587cc92e76528077b22
Ryan Schmidt <rms@rms80.com>
2018-06-05 21:29:23 -0400

    error checking in FResources

--- platform/FResources.cs
+++ platform/FResources.cs
@@ -12,6 +12,10 @@ namespace f3
         public static fMesh LoadMesh(string sPath)
         {
             Mesh mesh = Resources.Load<Mesh>(sPath);
+            if (mesh == null) {
+                DebugUtil.Log(2, "[FResources.LoadMesh] mesh not found at {0}", sPath);
+                return new fMesh(new g3.DMesh3());
+            }
             return new fMesh(mesh);
         }
 
@@ -19,6 +23,10 @@ namespace f3
         public static MemoryStream LoadBinary(string sPath)
         {
             TextAsset asset = Resources.Load(sPath) as TextAsset;
+            if (asset == null) {
+                DebugUtil.Log(2, "[FResources.LoadBinary] file not found at {0}", sPath);
+                return null;
+            }
             return new MemoryStream(asset.bytes);
         }
 
@@ -26,6 +34,10 @@ namespace f3
         public static string LoadText(string sPath)
         {
             TextAsset asset = Resources.Load(sPath) as TextAsset;
+            if (asset == null) {
+                DebugUtil.Log(2, "[FResources.LoadText] file not found at {0}", sPath);
+                return "";
+            }
             return asset.text;
         }
 
