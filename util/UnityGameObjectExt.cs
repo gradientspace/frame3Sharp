f8819f85142eb1390f886c950a9a4c2320aa89da
Ryan Schmidt <rms@rms80.com>
2018-06-11 09:45:09 -0400

    IsVisible() could return true even if object was not visible due to parent visibility.

--- util/UnityGameObjectExt.cs
+++ util/UnityGameObjectExt.cs
@@ -178,7 +178,7 @@ namespace f3
         }
         public static bool IsVisible(this GameObject go)
         {
-            return (go.activeSelf == true);
+            return (go.activeSelf == true && go.activeInHierarchy);
         }
 
 
